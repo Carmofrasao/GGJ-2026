db243b09d103d65af858b5571b8be1c15a02bfa6
AndreFelipeA <104396880+AndreFelipeA@users.noreply.github.com>
2026-01-31 04:19:32 -0300

    Add Double Jump, Glide

--- ggj/Character.cs
+++ ggj/Character.cs
@@ -3,23 +3,56 @@ using System;
 
 public partial class Character : CharacterBody2D
 {
-	[Export] public int Speed { get; set; } = 10;
+	[Export] public int Speed { get; set; } = 200;
 	
 	
-	[Export] public float JumpHeight;
-	[Export]public float JumpTimetoPeak;
-	[Export] public float JumpTimeToDescent;
+	[Export] public float JumpHeight = 110.0f;
+	[Export] public float JumpTimetoPeak = 0.5f;
+	[Export] public float JumpTimeToDescent = 0.35f;
 	
+	[Export] public float DoubleJumpHeightMultiplier = 1.1f;
+	[Export] public float DoubleJumpFallTimeMultiplier = 0.9f;
+	
+	[Export] public float GlideTimeToDescent = 3.0f;
+	
+	 public bool CanDoubleJump;
+	 public bool doubleJumpActive = false;
+	 public bool glideActive = false;
 	 public float JumpVelocity;
 	 public float JumpGravity;
 	 public float FallGravity;
 	
+	 // Minha maluquice
+	 public float DoubleJumpVelocity;
+	 public float DoubleJumpGravity;
+	 public float DoubleFallGravity;
+	
+	// Minha maluquice glide
+	public float GlideFallGravity;
+	public float DoubleGlideFallGravity;
+	
 	public override void _Ready()
 	{
-		AddToGroup("Player");
 		JumpVelocity = ((2.0f * JumpHeight) / JumpTimetoPeak) * -1.0f;
 		JumpGravity = ((-2.0f * JumpHeight) / (JumpTimetoPeak * JumpTimetoPeak)) * -1.0f;
 		FallGravity = ((-2.0f * JumpHeight) / (JumpTimeToDescent * JumpTimeToDescent)) * -1.0f;
+		
+		// Minha maluquice
+		// Double jump: 1.5x height, same time-to-peak => scale both v0 and gravity by height multiplier
+		float H2 = JumpHeight * DoubleJumpHeightMultiplier;
+
+		DoubleJumpVelocity = ((2.0f * H2) / JumpTimetoPeak) * -1.0f;
+		DoubleJumpGravity  = ((-2.0f * H2) / (JumpTimetoPeak * JumpTimetoPeak)) * -1.0f;
+
+		// Double fall: choose gravity so fall time scales by DoubleJumpFallTimeMultiplier
+		// g2 = g * (heightMult / timeMult^2)
+		float FallGravityMultiplier = DoubleJumpHeightMultiplier / (DoubleJumpFallTimeMultiplier * DoubleJumpFallTimeMultiplier);
+		DoubleFallGravity = FallGravity * FallGravityMultiplier;
+		
+		// Glide fall gravity (same height, longer descent time)
+		GlideFallGravity = ((-2.0f * JumpHeight) / (GlideTimeToDescent * GlideTimeToDescent)) * -1.0f;
+		DoubleGlideFallGravity = GlideFallGravity * FallGravityMultiplier;
+		
 	}
 	
 	public float GetInput()
@@ -37,15 +70,37 @@ public partial class Character : CharacterBody2D
 
 	public float GetGravity()
 	{
-		return Velocity.Y < 0.0f ? JumpGravity : FallGravity;
+		bool rising = Velocity.Y < 0.0f;
+
+		if (rising)
+			return doubleJumpActive ? DoubleJumpGravity : JumpGravity;
+
+		// falling:
+		if (doubleJumpActive)
+			return glideActive ? DoubleGlideFallGravity : DoubleFallGravity;
+
+		return glideActive ? GlideFallGravity : FallGravity;
 	}
 	
 	public void Jump()
 	{
-		GD.Print("Jump");
+		GD.Print("JumpGravity: ", JumpGravity);
+		GD.Print("FallGravity: ", FallGravity);
+		GD.Print("Jump Velocity: ", JumpVelocity);
 		Velocity = new Vector2(Velocity.X, JumpVelocity);
 	}
 	
+	public void DoubleJump()
+	{
+		if (!CanDoubleJump) return;
+		GD.Print("DoubleJump");
+
+		doubleJumpActive = true;
+		GD.Print("DoubleJumpGravity: ", DoubleJumpGravity);
+		GD.Print("DoubleFallGravity: ", DoubleFallGravity);
+		GD.Print("DoubleJumpVelocity: ", DoubleJumpVelocity);
+		Velocity = new Vector2(Velocity.X, DoubleJumpVelocity);
+	}
 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -58,8 +113,26 @@ public partial class Character : CharacterBody2D
 {
 	Velocity = new Vector2(GetInput() * Speed, Velocity.Y);
 	
+	if (IsOnFloor())
+	{
+		CanDoubleJump = true;
+		doubleJumpActive = false;
+		glideActive = false;
+	}
+	
 	if (Input.IsActionJustPressed("up") && IsOnFloor())
+	{
 		Jump();
+	}
+	else if (Input.IsActionJustPressed("up") && !IsOnFloor() && CanDoubleJump)
+	{
+		DoubleJump();
+		CanDoubleJump = false;
+	}
+	
+	// Glide
+	bool glideHeld = Input.IsActionPressed("glide");
+	glideActive = glideHeld && !IsOnFloor() && Velocity.Y > 0.0f;
 	
 	Velocity = new Vector2(
 		Velocity.X,
--- ggj/enemy.gd
+++ ggj/enemy.gd
@@ -2,15 +2,24 @@ extends CharacterBody2D
 
 
 var Speed = 100;
+var Gravity = 500;
 var PlayerChase = false;
 var Player = null;
+var Spawn;
+
 
 func _physics_process(delta: float) -> void:
-	if PlayerChase:
-		position += (Player.position - position).normalized() * Speed * delta
-		
-	move_and_collide(Vector2(0,0)) 
+	if PlayerChase and Player:
+		var dir = Player.global_position.x - global_position.x
+		velocity.x = sign(dir) * Speed
+	else:
+		velocity.x = lerp(velocity.x, 0.0, 0.1)
 	
+	move_and_collide(velocity) 
+	
+
+
+
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -22,3 +31,8 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	print("saiu");
 	Player = null;
 	PlayerChase = false;
+
+
+func _on_ready() -> void:
+	Spawn = position;
+	pass # Replace with function body.
