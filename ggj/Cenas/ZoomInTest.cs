a36de623f77d90d33b98168b6099e6ac4a697eed
Carmofrasao <anderfrasao52@gmail.com>
2026-02-01 01:29:14 -0300

    Fix Camera, Add Gate

--- ggj/Cenas/ZoomInTest.cs
+++ ggj/Cenas/ZoomInTest.cs
@@ -3,8 +3,12 @@ using System;
 
 public partial class ZoomInTest : Area2D
 {
-	[Export] public Camera2D camera;
+	[Export] public Camera camera;
 	[Export] public Character character { get; set; }
+	[Export] public Gate gate;
+	
+	private Vector2 cam5 = new Vector2(2432,  1088); // Cam5
+	private Vector2 cam4 = new Vector2(2406,  540); // Cam4
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -19,10 +23,21 @@ public partial class ZoomInTest : Area2D
 	private void OnBodyEntered(Node2D body)
 	{
 		if (body is not Character) return;
+		
+		camera.GlobalPosition = cam5;
+		camera.zoomIn = true;
+		camera.setZoom(new Vector2(5f, 5f));
+		
 	}
 	
 	private void OnBodyExited(Node2D body)
 	{
 		if (body is not Character) return;
+		
+		camera.GlobalPosition = cam4;
+		camera.zoomIn = false;
+		camera.setZoom(new Vector2(1f, 1f));
+		
+		gate.setGate();
 	}
 }
